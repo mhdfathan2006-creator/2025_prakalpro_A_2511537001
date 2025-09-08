2b9856f01fcfa0517288c3e008d54a73b9e9aa53
hp <hp@FTI-LAB03-PC11>
2025-09-08 15:06:55 +0700

    praktikum 1 kelas A

---
+++ prakalpro_2025_A_2511537001__/src/pekan1/MembuatKue.java
@@ -0,0 +1,34 @@
+package pekan1;
+
+public class MembuatKue {
+
+	public static void main(String[] args) {
+		makeButter();
+		memanggang();
+		memanggang();
+		menghias();
+		
+
+	}
+	
+	//Langkah 1: siapkan adonan
+	public static void makeButter() {
+		System.out.println("Campur bahan kering.");
+		System.out.println("Krim mentega dan gula.");
+		System.out.println("Kocok telurnya.");
+		System.out.println("Masukkan bahan kering.");
+	}
+	//Langkah 2: Panggang sekumpulan kue
+	public static void memanggang() {
+		System.out.println("Setel suhu oven.");
+		System.out.println("Setel pengatur suhu.");
+		System.out.println("Masukkan kue ke dalam oven.");
+		System.out.println("Biarkan cookie untuk di panggang.");
+		
+	}
+	//Langkah 3: Hiasi cookie
+	public static void menghias() {
+			System.out.println("Campur bahan untuk frosting.");
+			System.out.println("Taburkan frosting untuk dan taburan.");
+		}
+}
