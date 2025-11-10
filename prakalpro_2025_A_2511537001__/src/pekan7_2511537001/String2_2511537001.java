42c977a1c5107b8f723c422b1b5e6c6d98ad5869
user <user@10.44.8.223>
2025-11-10 14:45:04 +0700

    pekan 7 alpro

--- prakalpro_2025_A_2511537001__/src/pekan7_2511537001/String2_2511537001.java
+++ prakalpro_2025_A_2511537001__/src/pekan7_2511537001/String2_2511537001.java
@@ -3,7 +3,29 @@ package pekan7_2511537001;
 public class String2_2511537001 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		String firstName = "Syifa";
+		String lastName = "Muhassanah";
+		String txt1 = "Dosen\"intelektual\"kampus";
+		System.out.println("Nama Lengkap: "+firstName + " " +lastName); 
+		System.out.println("Nama Lengkap: "+firstName.concat(lastName)); 
+		System.out.println(txt1);
+		int x = 10;
+		int y = 20;
+		int z = x + y;
+		System.out.println("x+ y= "+z);
+		String a = "10";
+		String b = "20";
+		String c = a + b;
+		System.out.println("String a + string b ="+c);
+		String v = a + y;
+		System.out.println("String a + integer y ="+v);
+		
+		
+		
+		
+		
+		
+		
 
 	}
 
